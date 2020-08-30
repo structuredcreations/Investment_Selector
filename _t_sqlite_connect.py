307c9c8f6cfce3224f389c907cb623de97228505
Joe Zhou <JZ@Joes-MacBook-Air.local>
2020-08-30 13:55:04 +0800

    testing database connection

---
+++ _t_sqlite_connect.py
@@ -0,0 +1,10 @@
+
+
+# import sqlite3
+
+# conn = sqlite3.connect('/Users/JZ/Downloads/Databases/test_database.db')
+
+# from sqlalchemy import create_engine
+
+
+pip install pandas==1.0.3 sqlalchemy==1.3.15
